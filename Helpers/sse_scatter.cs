8f6044dd33e03698476ac70799135ba4ca3d556f
sdorodni <sergey.dorodnicov@intel.com>
2018-04-03 02:28:43 -0700

    Removing redundant typenames

--- Helpers/sse_scatter.cs
+++ Helpers/sse_scatter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace SSE_Scatter
 {
@@ -61,11 +62,69 @@ namespace SSE_Scatter
             }
         }
 
+        static void PrintAVX_Scatter(int size)
+        {
+            for (int gap = 2; gap < size; gap++)
+            {
+                for (int offset = 0; offset < gap; offset++)
+                {
+                    int[] permutation = new int[gap * gap * 6];
+                    uint[] mask = new uint[gap * gap * 6];
+                    for (int i = 0; i < 8; i++)
+                    {
+                        permutation[gap * i + offset] = i;
+                        mask[gap * i + offset] = 1;
+                    }
+
+                    for (int line = 0; line < gap; line++)
+                    {
+                        int[] bits = new int[8];
+                        uint m = 0;
+                        for (int i = 0; i < 8; i++)
+                        {
+                            bits[i] = permutation[line * 8 + i];
+                        }
+
+                        m = (mask[line * 4] * 0xff) +
+                            ((mask[line * 4 + 1] * 0xff) << 8) +
+                            ((mask[line * 4 + 2] * 0xff) << 16) +
+                            ((mask[line * 4 + 3] * 0xff) << 24);
+
+                        Console.WriteLine("SET_SCATTER_SHUFFLE(" + gap + ", " + offset + ", " + line +
+                            ", _MM_SHUFFLE(" + string.Join(", ", 
+                            bits.Reverse()
+                            ) + "), 0x"
+                            + m.ToString("X8") + ");");
+
+                        int[] gbits = new int[8];
+                        uint[] gmaks = new uint[8];
+
+                        for (int i = 0; i < 8; i++)
+                        {
+                            if (mask[i] != 0)
+                            {
+                                gbits[bits[i]] = i;
+                                gmaks[bits[i]] = 0xff;
+                            }
+                        }
+
+                        m = (gmaks[0] * 0xff) +
+                            ((gmaks[1] * 0xff) << 8) +
+                            ((gmaks[2] * 0xff) << 16) +
+                            ((gmaks[3] * 0xff) << 24);
+
+                        Console.WriteLine("SET_GATHER_SHUFFLE(" + gap + ", " + offset + ", " + line +
+                            ", _MM_SHUFFLE(" + string.Join(", ", gbits.Reverse()) + "), 0x"
+                            + m.ToString("X8") + ");");
+                    }
+                }
+            }
+        }
 
 
         static void Main(string[] args)
         {
-            PrintSSE_Scatter(6);
+            PrintAVX_Scatter(6);
         }
     }
 }
